0b3eebdafe01a4c1c8d9ae925019a71ead5593aa
AdnanGulzar <adnangul9900@gmail.com>
2023-02-19 14:40:28 +0500

    first commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <section className='contact'>
+    <div className="center">
+    <p>There’s never been a better time to start.</p>
+   <h1>The time is now.</h1>
+   <button>
+    Talk Us
+   </button>
+    </div>
+
+    
+    
+    </section>
+  )
+}
+
+export default Contact
+
+
---
+++ src/components/Patternbg.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Patternbg = () => {
+  return (
+    <section className='patternBg'>
+    <div className="card1">
+    For those who know they can be more successful, the time is now.
+    </div>
+    <div className="card2">
+    <p>
+    The Growth Academy gives business leaders the tools to implement a blueprint for attaining growth. Using a unique and proven methodology, we enable you to create a healthier and efficient team that will transform sales and break through barriers to let your business thrive.
+    </p>
+    
+    <button>Learn More</button>
+    </div>
+    
+    
+    
+    </section>
+  )
+}
+
+export default Patternbg
